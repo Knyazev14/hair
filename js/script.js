05d4d28db9b8ce10510278d51c937b36ee915ce6
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2022-03-08 14:59:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-
-
+'use strict'
+//================================
+//Burger
+const menuIcon = document.querySelector('.menu__icon');
+if(menuIcon){
+	const menuBody = document.querySelector('.header-menu');
+	menuIcon.addEventListener("click", function(){
+	menuIcon.classList.toggle("_active");
+	menuBody.classList.toggle("_active");
+});
+}
+//================================
 
 
 
