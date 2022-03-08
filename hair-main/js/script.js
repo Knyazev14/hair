b36fd7de497fb8f4a196a93cb40d180f82756e29
Knyazev14 <97404941+Knyazev14@users.noreply.github.com>
2022-03-09 00:15:29 +0300

    Add files via upload

---
+++ hair-main/js/script.js
@@ -0,0 +1,16 @@
+'use strict'
+//Burger=================================================
+const menuIcon = document.querySelector('.menu__icon');
+if(menuIcon){
+	const menuBody = document.querySelector('.header-menu');
+	menuIcon.addEventListener("click", function(){
+	menuIcon.classList.toggle("_active");
+	menuBody.classList.toggle("_active");
+});
+}
+//=======================================================
+
+
+
+
+
